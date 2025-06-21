8da9cbc6f50a9d2e9f10f725557779ad74cd1964
Artem <128389053+TemchikPlayBoy@users.noreply.github.com>
2025-06-21 15:13:31 +1000

    Update db.php

--- db.php
+++ db.php
@@ -30,14 +30,5 @@
         echo "Не удалось создать таблицу Users";
     }
 
-    $sql = "CREATE TABLE IF NOT EXISTS posts( 
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(20) NOT NULL,
-    main_text VARCHAR(400) NOT NULL)";
-
-    if(!mysqli_query($link, $sql)) {
-        echo "Не удалось создать таблицу posts";
-    }
-
     mysqli_close($link);
 ?>
