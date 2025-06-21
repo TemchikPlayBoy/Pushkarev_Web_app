6c76775a6788ad698a04103be0a081a946bc6e3c
Artem <128389053+TemchikPlayBoy@users.noreply.github.com>
2025-06-21 16:58:59 +1000

    наверное итоговые изменения

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Приветствие - <?= htmlspecialchars($user['username']) ?></title>
+    <title>Приветствие</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLWZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
@@ -11,7 +11,7 @@
 <body>
     <div class="container">
         <div class="welcome-container text-center">
-            <h1>Привет, <span class="text-primary"><?= htmlspecialchars($user['username']) ?></span>!</h1>
+            <h1>Привет, <span class="text-primary"><?php echo htmlspecialchars($_COOKIE['User'] ?? 'GUEST'); ?></span>!</h1>
             <p class="lead">Добро пожаловать в вашу учетную запись</p>
             
             <div class="mt-4">
@@ -36,7 +36,7 @@ $sql = "SELECT * FROM users WHERE username='$login'";
 $result = mysqli_query($link, $sql);
 
 if (!$result || mysqli_num_rows($result) == 0) {
-    setcookie("User", "", time() - 3600); // Удаляем куку если пользователь не найден
+    setcookie("User", "", time() - 3600);
     header("Location: /login.php");
     exit();
 }
